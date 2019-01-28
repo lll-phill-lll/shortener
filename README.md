6ea6e8b26e80f452cc1175371bce7f1f8b39980e
Mikhail <mefilitov@edu.hse.ru>
2019-01-29 02:21:18 +0300

    added simple handlers

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Microservice that shortens link
 ## Quickstart
 To install all the dependencies you need [dep](https://github.com/golang/dep). 
 Install it: `curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh`.
+
 Run: `dep ensure`.
--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/lll-phill-lll/shortener/logger"
+	"net/http"
 	"os"
 )
 
 func InitApp() {
 	logger.SetLogger(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
-
 }
 
+func short(w http.ResponseWriter, r *http.Request) {
+	logger.Debug.Println("short")
+}
+
+
+func hash(w http.ResponseWriter, r *http.Request) {
+	logger.Debug.Println("hash")
+}
+
+
 func main() {
-	logger.Info.Println("Hello, world!")
-	logger.Info.Println("Hello, world!")
+	InitApp()
+	r := mux.NewRouter()
+	r.HandleFunc("/short", short).Methods("POST")
+	r.HandleFunc("/{hash}", hash).Methods("GET")
+	http.Handle("/", r)
+
+	logger.Info.Println("Start Listening on port 8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		logger.Error.Println(err.Error())
+	}
 }
